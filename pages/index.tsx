cb2e4d0332252fadc93722caaa94a0b3adcf6ca3
urbano <urbanobaz@yahoo.com>
2023-01-08 20:36:11 -0500

    Further tying everything in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import { Quicksand } from '@next/font/google'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
 
 
-const inter = Inter({ subsets: ['latin'] })
+const quicksand = Quicksand({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,9 +16,15 @@ export default function Home() {
         <meta name="description" content="Urbano's portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+          <script
+            defer
+            src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"
+          />
       </Head>
-      <Main />
-      <Projects />
+      <main className={quicksand.className}>
+        <Main />
+        <Projects />
+      </main>
     </>
   )
 }
