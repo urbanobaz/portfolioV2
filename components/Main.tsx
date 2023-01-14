08f40b31bbedbe333e6a77e9f585a31404f24432
Urbano <urbanobaz@yahoo.com>
2023-01-14 15:24:02 -0500

    still trying fix for build netlify issue

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import headshot from "../public/finalheadshotedit.png";
 import styles from "../styles/Home.module.css";
